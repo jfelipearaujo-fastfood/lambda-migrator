4c6e81bfc72ad450009c500e526f033e9703c561
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2024-02-26 10:47:50 -0300

    fix: :bug: change name of sm

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -6,7 +6,7 @@ resource "random_password" "database_password" {
 }
 
 resource "aws_secretsmanager_secret" "sm_database_credentials" {
-  name = "sm-${var.project_name}-cred"
+  name = "sm-${var.project_name}"
 }
 
 resource "aws_secretsmanager_secret_version" "sm_database_credentials_version" {
