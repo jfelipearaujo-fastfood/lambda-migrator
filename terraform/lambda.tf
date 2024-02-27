7768c4d9905003e0cff29bf1ff12bf9140e0f15b
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2024-02-27 17:51:13 -0300

    feat: :sparkles: change password setup

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "lambda_function" {
       DB_HOST = module.db.db_instance_endpoint
       DB_NAME = var.database_name
       DB_USER = local.db_username
-      DB_PASS = jsondecode(aws_secretsmanager_secret_version.superuser.secret_string)["password"]
+      DB_PASS = module.db.db_instance_password
     }
   }
 
