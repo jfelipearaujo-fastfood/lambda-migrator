18394b219fd6454807f6c6542e9bf216940fcae9
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2024-03-16 09:22:16 -0300

    feat: :sparkles: add host address to the secret

--- terraform/modules/database/secret.tf
+++ terraform/modules/database/secret.tf
@@ -6,11 +6,14 @@ resource "random_password" "password" {
 
 resource "aws_secretsmanager_secret" "master_user_secret" {
   name = "db-${var.db_name}-secret"
+
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "master_user_secret_version" {
   secret_id = aws_secretsmanager_secret.master_user_secret.id
   secret_string = jsonencode({
+    host     = aws_db_instance.db.address,
     username = var.db_username,
     password = random_password.password.result
   })
