23fed1129962d1f764c3a56cc17edde4e6f58a5b
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2024-05-14 23:19:10 -0300

    feat: :sparkles: update terraform

---
+++ terraform/modules/migrator/secret.tf
@@ -0,0 +1,32 @@
+data "aws_secretsmanager_secret" "db_products_url_secret" {
+  name = "db-products-url-secret"
+}
+
+data "aws_secretsmanager_secret_version" "db_products_url_secret_version" {
+  secret_id = data.aws_secretsmanager_secret.db_products_url_secret.arn
+}
+
+data "aws_secretsmanager_secret" "db_orders_url_secret" {
+  name = "db-orders-url-secret"
+}
+
+data "aws_secretsmanager_secret_version" "db_orders_url_secret_version" {
+  secret_id = data.aws_secretsmanager_secret.db_orders_url_secret.arn
+}
+
+data "aws_secretsmanager_secret" "db_payments_url_secret" {
+  name = "db-payments-url-secret"
+}
+
+data "aws_secretsmanager_secret_version" "db_payments_url_secret_version" {
+  secret_id = data.aws_secretsmanager_secret.db_payments_url_secret.arn
+}
+
+data "aws_secretsmanager_secret" "db_productions_url_secret" {
+  name = "db-productions-url-secret"
+}
+
+data "aws_secretsmanager_secret_version" "db_productions_url_secret_version" {
+  secret_id = data.aws_secretsmanager_secret.db_productions_url_secret.arn
+}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@ variable "tags" {
   type        = map(string)
   description = "The default tags to use for AWS resources"
   default = {
-    App = "database"
+    App = "lambda"
   }
 }
 
@@ -12,24 +12,8 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "vpc_id" {
+variable "vpc_name" {
   type        = string
-  description = "The ID of the VPC"
-}
-
-variable "vpc_cidr_block" {
-  type        = string
-  description = "The CIDR block of the VPC"
-  default     = "10.0.0.0/16"
-}
-
-variable "private_subnets" {
-  type        = list(string)
-  description = "The IDs of the private subnets"
-}
-
-variable "db_username" {
-  type        = string
-  description = "The username for the database"
-  default     = "fastfood"
+  description = "The name of the VPC"
+  default     = "vpc-fastfood"
 }
